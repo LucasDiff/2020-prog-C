1ef38050866c7d4b8b9174352b6cdb5d07fa1431
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-03-05 11:25:04 +0100

    Update main.c

--- ps1/main.c
+++ ps1/main.c
@@ -23,5 +23,13 @@ free(reversed);
 encrypted = vigenere_encrypt("CoMPuTeR", "Hello world!");
 printf("%s\n", encrypted);
 free(encrypted);
+unsigned char* encrypted;
 
+// basic test with long text
+encrypted = bit_encrypt("Hello world!");
+for(int i=0; i < 12;i++) {
+    printf("%x ", encrypted[i]);
+    //80 9c 95 95 96 11 bc 96 b9 95 9d 10
+}
+free(encrypted);
 }

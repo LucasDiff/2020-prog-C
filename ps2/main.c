72dbbf5d0dd34da0b9d8b3a2005154d4a47f982f
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-03-18 22:20:26 +0100

    Update main.c

---
+++ ps2/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "k.c"
+#include "k.h"
+#include "hof.c"
+#include "hof.h"
+#include "ui.c"
+#include "ui.h"
+
+int main(){
+    struct game game = {
+    .board = {
+        {'A', 'B', 'C', 'D'},
+        {'E', 'F', 'G', 'H'},
+        {'I', 'J', 'K', 'A'},
+        {'B', 'C', 'D', 'E'}
+    },
+    .score = 0
+};
+
+printf("is won: %d\n", is_game_won(game));
+
+printf("is move possible: %d\n", is_move_possible(game));
+
+return 0;
+}

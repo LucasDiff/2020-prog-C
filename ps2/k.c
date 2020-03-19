196833833bcfb557cc3423698af0d7364e4e7a22
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-03-19 06:13:36 +0100

    Update k.c

--- ps2/k.c
+++ ps2/k.c
@@ -6,22 +6,6 @@
 
 #include "k.h"
 
-//void add_random_tile(struct game *game){
-    int row, col;
-    // find random, but empty tile
-    do{
-        row = rand() % SIZE;
-        col = rand() % SIZE;
-    }while(game->board[row][col] != ' ');
-
-    // place to the random position 'A' or 'B' tile
-    if(rand() % 2 == 0){
-        game->board[row][col] = 'A';
-    }else{
-        game->board[row][col] = 'B';
-    }
-}
-
 
 bool is_game_won(const struct game game){
     for (int vyska = 0; vyska < 3; vyska++){

5e9e8625abe48044910491fa8fa50db6aeae9e1d
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-03-20 17:45:38 +0100

    Update k.c

--- ps2/k.c
+++ ps2/k.c
@@ -37,7 +37,7 @@ bool is_move_possible(const struct game game){
             if (game.board[vyska][sirka] == game.board[vyska][sirka - 1]){
                 return true;
             }
-}
+}    
 }
     return false;
 }
@@ -52,7 +52,7 @@ bool update(struct game *game, int dy, int dx){
     if ( dy < -1){
     return false;
     }
-    if ( dy > 1){
+    if ( dy >= 2){
     return false;
     }
     if ( dx < -1){

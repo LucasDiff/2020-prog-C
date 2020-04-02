871b22327d7397ead21c40987090b17e13484e40
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-04-02 17:08:54 +0200

    Update mm.c

--- ps3/mm.c
+++ ps3/mm.c
@@ -4,21 +4,28 @@
 #include <string.h>
 
 int main (){
-long int c;
-scanf("%ld",&c);
-int v = 0;
-int a = 0;
-while ( c >= 10){
-while ( c >= 10){
-a++;
-v = v + ( c % (10));
-c = c - ( c % (10) );
-c = c / 10;
-}
-v = v + c;
-c = v;
-v = 0;
-}
-printf ("%ld\n", c );
-return c;
+    int g = 0;
+    printf("Zadaj cislo\n");
+    char c[100000];
+    scanf("%s",c);
+    int v = 0;
+    int a = 0;
+    while(c[a] != '\0'){
+    a++;
+    }
+    for (int b = 0; b < a - 1; b++){
+        v = v + c[b] - 48;
+    }
+        while ( v >= 10){
+            while ( v >= 10){
+            g = g + ( v % (10));
+            v = v - ( v % (10) );
+            v = v / 10;
+            }
+        g = g + v;
+        v = g;
+        g = 0;
+        }
+    printf ("%d\n", v );
+    return v;
 }

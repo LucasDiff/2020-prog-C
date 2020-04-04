7bef1e838b594a7ea8e6f955bcb9dd041f66ca41
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-04-04 12:31:16 +0200

    Update racer.c

--- ps3/racer.c
+++ ps3/racer.c
@@ -4,6 +4,19 @@
 #include <string.h>
 
 int main (){
-    printf("3.000000000");
-    return 0;
+    double n = 0;
+    double t = 0;
+    double s = 0;
+    double v = 0;
+    double medzi = 0;
+    double k = 0;
+    scanf("%lf",&n);
+    scanf("%lf",&t);
+    for (int c = 0; c <= n - 1; c++){
+        scanf("%lf",&s);
+        scanf("%lf",&v);
+        medzi = s / (t / (n));
+        k = k + (medzi - v) / n;
+    }
+    printf("%.10f\n",k);
 }

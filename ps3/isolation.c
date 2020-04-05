2b80bad762f2558ec70c38be41d251e7691d8f0e
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-04-05 11:40:27 +0200

    Update isolation.c

--- ps3/isolation.c
+++ ps3/isolation.c
@@ -4,7 +4,18 @@
 #include <string.h>
 
 int main (){
+    int a = 0;
+    int b = 0;
     scanf("%d",&a);
-    printf("%d",a - 1);
+    scanf("%d",&b);
+    if (a == 1){
+    printf("%d\n",a);
+    return 0;
+    }
+    if ( b == 1){
+    printf("%d\n",a);
+    return 0;
+    }
+    printf("2\n");
     return 0;
 }

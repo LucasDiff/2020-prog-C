b8ea97c1deb63a4323e2b1fd1722286db1b39ace
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-03 19:49:37 +0200

    Update transformations.c

--- ps4/transformations.c
+++ ps4/transformations.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
-#include "bmp.h"
-#include <string.h>
-int main (){
-printf("Hello transformations");
-return 0;
+#include <memory.h>
+#include <stdlib.h>
+#include <math.h>
+#include "transformations.h"
+
+struct bmp_image* crop(const struct bmp_image* image, const uint32_t start_y, const uint32_t start_x, const uint32_t height, const uint32_t width);
+struct bmp_image* extract(const struct bmp_image* image, const char* colors_to_keep);
+struct bmp_image* flip_vertically(const struct bmp_image* image);
+struct bmp_image* scale(const struct bmp_image* image, float factor);
+struct bmp_image* flip_horizontally(const struct bmp_image* image);
+struct bmp_image* rotate_left(const struct bmp_image* image);
+struct bmp_image* rotate_right(const struct bmp_image* image);
+
+
+struct bmp_image* scale(const struct bmp_image* image, float factor) {
+    return NULL;
+}
+struct bmp_image* flip_horizontally(const struct bmp_image* image) {
+    return NULL;
+}
+struct bmp_image* flip_vertically(const struct bmp_image* image) {
+    return NULL;
+}
+
+struct bmp_image* rotate_left(const struct bmp_image* image) {
+    return NULL;
+}
+
+struct bmp_image* crop(const struct bmp_image* image, const uint32_t start_y, const uint32_t start_x, const uint32_t height, const uint32_t width) {
+    return NULL;
+}
+
+struct bmp_image* rotate_right(const struct bmp_image* image) {
+    return NULL;
 }

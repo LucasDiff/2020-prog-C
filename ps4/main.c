cb9e5fa38ae55389a6ae0530d4f59c6e0487b060
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-03 20:21:18 +0200

    Update main.c

--- ps4/main.c
+++ ps4/main.c
@@ -1,7 +1,22 @@
-include <stdio.h>
+#include <stdio.h>
 #include "bmp.h"
-#include <string.h>
-int main (){
-printf("Hello main");
-return 0;
+#include "bmp.c"
+#include "transformations.c"
+#include "transformations.h"
+#include <malloc.h>
+
+int main() {
+    int c = 0;
+    int v = 11;
+    if (v == 11){
+    flip_vertically(NULL);
+    free_bmp_image(NULL);
+    write_bmp(NULL, NULL);
+    rotate_right(NULL);
+    scale(NULL, c);
+    crop(NULL, c, c, c, c);
+    rotate_left(NULL);
+    flip_horizontally(NULL);
+    read_bmp(NULL);
+    }
 }

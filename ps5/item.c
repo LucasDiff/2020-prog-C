efd1659f8f3bed6d430b8c59fec517ed42fd8ab0
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-10 22:40:46 +0200

    Update item.c

---
+++ ps5/item.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"item.h"
+
+
+struct item* destroy_item(struct item* item){
+	free(item->name);
+	free(item->description);
+	free(item);
+	return NULL;
+}

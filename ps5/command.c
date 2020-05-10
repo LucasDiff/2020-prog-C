4a4712cac1a590d382d5e74f2add25da32c3839a
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-10 22:39:33 +0200

    Update command.c

---
+++ ps5/command.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"command.h"
+
+struct command* destroy_command(struct command* command){
+	free(command->name);
+	free(command->description);
+	free(command);		
+
+	return NULL;
+}

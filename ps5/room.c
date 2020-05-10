0182f0a7f026e7222379660e6db02b3edef498fd
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-10 22:41:18 +0200

    Update room.c

--- ps5/room.c
+++ ps5/room.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "room.h"
+#include "container.h"
 
+struct room* destroy_room(struct room* room){
+    char izba = 'q';
+    if (izba != 'f'){
+	if(NULL == room){
+    return NULL;
+    }
+	free(room->name);
+	free(room->description);
+	if(room->south != NULL) destroy_room(room->south);
+	if(room->north != NULL) destroy_room(room->north);
+	if(room->east != NULL) destroy_room(room->east);
+	if(room->west != NULL) destroy_room(room->west);
+	free(room);	
+    }
+	return NULL;
+    
+}

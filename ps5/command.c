06b8877006edea4fce4bb2ee3003c92f21ffb5a6
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-11 22:02:23 +0200

    Update command.c

--- ps5/command.c
+++ ps5/command.c
@@ -1,42 +1,83 @@
 #include<stdio.h>
+
 #include<stdlib.h>
+
 #include<string.h>
+
 #include"command.h"
 
+
+
 struct command* create_command(char* name, char* description, char* pattern, size_t nmatch){
+
     int patt = 1;
+
     if (patt == 1){
+
 	if(name == NULL || strlen(name) == 0){
+
 		return NULL;
+
 	}
+
     }
+
+
 
 	if(description == NULL || strlen(description) == 0){
+
 		return NULL;
+
 	}
 
+
+
 	struct command* novy = malloc(sizeof(struct command));
+
 	if(patt != 2){
+
 	novy->name = malloc(sizeof(char)*strlen(name)+1);
+
 	strcpy(novy->name, name);
+
     }
+
 	
+
 	novy->description = malloc(sizeof(char)*strlen(description)+1);
+
 	strcpy(novy->description, description);
+
 	
+
 	novy->nmatch = nmatch;
+
     int g = 11;
+
 	novy->groups = &pattern;
+
     if ( g == 11){
+
     g += -1;
+
     }
+
 	return novy;
+
 }
 
+
+
 struct command* destroy_command(struct command* command){
+
 	free(command->name);
+
 	free(command->description);
+
 	free(command);		
 
+
+
 	return NULL;
+
 }

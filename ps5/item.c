ae562eab0e138f9dc431f0387d290690b06da35c
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-11 22:03:54 +0200

    Update item.c

--- ps5/item.c
+++ ps5/item.c
@@ -3,6 +3,23 @@
 #include<string.h>
 #include"item.h"
 
+struct item* create_item(char* name, char* description, unsigned int properties){
+    char meno = ' ';
+    if ( meno == ' '){
+	if(NULL == name || NULL == description){
+    return NULL;
+    }
+	if(strlen(name) == 0 || strlen(description) == 0){
+    return NULL;
+    }
+    }
+	struct item* bod = malloc(sizeof(struct item));
+	bod->name = name;
+	bod->description = description;
+	bod->properties = properties;
+	return bod;
+}
+
 
 struct item* destroy_item(struct item* item){
 	free(item->name);

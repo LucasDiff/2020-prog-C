11fa16d08b10a2b7d08c59591cc20c3c2ac54391
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-13 11:26:04 +0200

    Update item.c

--- ps5/item.c
+++ ps5/item.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include "item.h"
 struct item* create_item(char* name, char* description, unsigned int properties){
     char meno = ' ';
     if ( meno == ' '){

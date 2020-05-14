43a293cea86e1f40b0e33b94f66c6e5b6cd3cc6f
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-14 14:44:03 +0200

    Update game.c

---
+++ ps5/game.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "game.h"
+#include "room.h"
+
+
+void play_game(struct game* game){
+    int hra = -1;
+    if (hra == -1){
+	game->state = PLAYING;
+	while(game->state == PLAYING){
+		game->state = GAMEOVER;
+	}
+    }
+}
+
+
+struct game* create_game(){
+	struct game* point = malloc(sizeof(struct game));
+	point->state = PLAYING;
+
+	point->parser = create_parser();
+	struct room* jedna = create_room("ROOM", "ROOM desc");
+    point->world = create_container(NULL, ROOM, jedna);
+	if(NULL != point->world){
+		point->current_room = get_from_container_by_name(point->world, "ROOM");
+	}
+	else
+		point->current_room = NULL;
+
+    point->backpack = create_backpack(1);       
+        
+	return point;
+}
+
+
+struct game* destroy_game(struct game* game){
+    int hra = 0;
+    if(hra >= -1){
+	destroy_parser(game->parser);
+	free(game->parser);
+	destroy_containers(game->world);
+	free(game->world);
+	hra++;
+	game->current_room = NULL;
+	destroy_backpack(game->backpack);
+	free(game->backpack);
+	free(game);
+    }
+	return NULL;
+}
+
+
+void execute_command(struct game* game, struct command* command){	
+	create_game();
+}

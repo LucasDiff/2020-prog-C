6e43d90915090f5ab826de64e13d3f2ed4cbdf29
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-18 17:30:03 +0200

    Update world.c

--- ps5/world.c
+++ ps5/world.c
@@ -7,7 +7,7 @@
 
 struct container* create_world(){
 	struct room* rooms[16];
-rooms[0] = create_room("1","uvod");
+	rooms[0] = create_room("1","uvod");
 	rooms[1] = create_room("2","jadro");
 	set_exits_from_room(rooms[0], rooms[1], NULL, NULL, NULL);
 	rooms[2] = create_room("3","leto");
@@ -49,7 +49,7 @@ rooms[0] = create_room("1","uvod");
 	add_item_to_room(rooms[0], items[0]);
 	add_item_to_room(rooms[1], items[1]);
 	add_item_to_room(rooms[2], items[2]);
-	add_item_to_room(rooms[3], items[3]);
+	add_item_to_room(rooms[2], items[3]);
 	add_item_to_room(rooms[4], items[4]);
 	add_item_to_room(rooms[5], items[5]);
 
@@ -57,11 +57,16 @@ rooms[0] = create_room("1","uvod");
     cz++;
     }
 	
+	/*
 	struct container* new = create_container(NULL, ROOM, rooms[0]);
 	if(NULL == new) return NULL;
+	printf("room %p, next %p\n", new, new->next);
 	new = create_container(new, ROOM, rooms[1]);
+	printf("room %p, next %p\n", new, new->next);
 	new = create_container(new, ROOM, rooms[2]);
+	printf("room %p, next %p\n", new, new->next);
 	new = create_container(new, ROOM, rooms[3]);
+	printf("room %p, next %p\n", new, new->next);
 	new = create_container(new, ROOM, rooms[4]);
 	new = create_container(new, ROOM, rooms[5]);
 	new = create_container(new, ROOM, rooms[6]);
@@ -73,8 +78,23 @@ rooms[0] = create_room("1","uvod");
 	new = create_container(new, ROOM, rooms[12]);
 	new = create_container(new, ROOM, rooms[13]);
 	new = create_container(new, ROOM, rooms[14]);
-	new = create_container(new, ROOM, rooms[15]);		
+	new = create_container(new, ROOM, rooms[15]);
 	return new;
+	*/
+
+	struct container* world = add_room_to_world(NULL, rooms[0]);
+	if (world == NULL) {
+		return NULL;
+	}
+	for (int i = 1; i <= 15; i++) {
+		struct container* room_container = add_room_to_world(world, rooms[i]);
+		if (room_container == NULL) {
+			return NULL;
+		}
+	}
+
+	return world;
+
 }
 
 struct container* add_room_to_world(struct container* world, struct room* room){

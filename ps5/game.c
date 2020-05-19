55ed5a75fa0ecf9de274788a932e44e21dc6cf3f
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-05-19 11:27:23 +0200

    Update game.c

--- ps5/game.c
+++ ps5/game.c
@@ -4,39 +4,64 @@
 #include "game.h"
 #include "room.h"
 #include "container.h"
+#include "world.h"
 
 
 void play_game(struct game* game){
     int hra = -1;
     if (hra == -1){
-        
-        if (game == NULL) {
-            return;
-        }
-	game->state = PLAYING;
-	while(game->state == PLAYING){
-		game->state = GAMEOVER;
-	}
+    
+    if (game == NULL) {
+    	return;
+    }
+	
+	char input[256];
+
+	do {
+		game->state = PLAYING;
+		printf("\n*****************************\n");
+		printf("Hra sa zacina. Zadaj prikazy.\n");
+		printf("*****************************\n");
+		while(game->state == PLAYING){
+			show_room(game->current_room);
+			printf("\n> ");
+			scanf("%s", input);
+			struct command* cmd = parse_input(game->parser, input);
+			if (cmd != NULL) {
+				execute_command(game, cmd);
+			} else {
+		    	printf("*** Neznamy prikaz\n");
+			}
+		}
+	} while (game->state == RESTART);
+	
+	if (game->state == SOLVED) {
+		printf("\n **** Gratulujem! Porazil si zleho druka ***\n\n");
+	} else if (game->state == GAMEOVER) {
+		printf("\n **** Ajajaj. Bol si porazeny drukom ***\n\n");
+	} 
+
     }
 }
 
 
 struct game* create_game(){
-	struct game* point = malloc(sizeof(struct game));
-	point->state = PLAYING;
+	struct game* new_game = malloc(sizeof(struct game));
+	new_game->state = PLAYING;
 
-	point->parser = create_parser();
-	struct room* jedna = create_room("ROOM", "ROOM desc");
-    point->world = create_container(NULL, ROOM, jedna);
-	if(NULL != point->world){
-		point->current_room = get_from_container_by_name(point->world, "ROOM");
+	new_game->parser = create_parser();
+
+    new_game->world = create_world();
+	if (NULL != new_game->world){
+		new_game->current_room = new_game->world->room;
 	}
-	else
-		point->current_room = NULL;
+	else {
+		new_game->current_room = NULL;
+	}
 
-    point->backpack = create_backpack(1);       
+    new_game->backpack = create_backpack(1);       
         
-	return point;
+	return new_game;
 }
 
 
@@ -61,20 +86,46 @@ struct game* destroy_game(struct game* game)
 }
 
 
-void execute_command(struct game* game, struct command* command){
-    if (game == NULL || command == NULL) {
-        return;
+void execute_command(struct game* game, struct command* command) {
+	if (game == NULL || command == NULL) {
+		return;
+	}
+
+    if(strcasecmp(command->name, "KONIEC") == 0) {
+	    game->state = GAMEOVER;
+    } else if(strcasecmp(command->name, "RESTART") == 0) {
+		game->current_room = game->world->room;
+		// TODO: doplnit reset backpacku a rozlozenia itemov v miestnostiach
+	    game->state = RESTART;
+    } else if(strcasecmp(command->name, "SEVER") == 0) {
+	    if (game->current_room->north != NULL) {
+	    	game->current_room = game->current_room->north;
+	    } else {
+	    	printf("Na sever sa ist neda\n");
+	    }
+    } else if(strcasecmp(command->name, "JUH") == 0) {
+	    if (game->current_room->south != NULL) {
+	    	game->current_room = game->current_room->south;
+	    } else {
+	    	printf("Na juh sa ist neda\n");
+	    }
+    } else if(strcasecmp(command->name, "VYCHOD") == 0) {
+	    if (game->current_room->east != NULL) {
+	    	game->current_room = game->current_room->east;
+	    } else {
+	    	printf("Na vychod sa ist neda\n");
+	    }
+    } else if(strcasecmp(command->name, "ZAPAD") == 0) {
+	    if (game->current_room->west != NULL) {
+	    	game->current_room = game->current_room->west;
+	    } else {
+	    	printf("Na zapad sa ist neda\n");
+	    }
+    } else {
+    	printf("*** Nepodporovany prikaz\n");
     }
-    if(strcasecmp(command->name, "GAMEOVER") == 0){
-    game->state = GAMEOVER;
-    }
-    if(strcasecmp(command->name, "PLAYING") == 0){
-    game->state = PLAYING;
-    }
-    if(strcasecmp(command->name, "SOLVED") == 0){
-    game->state = SOLVED;
-    }
-    if(strcasecmp(command->name, "RESTART") == 0){
-    game->state = RESTART;
-    }
+
+	if (strcasecmp("16", game->current_room->name) == 0) {
+		game->state = SOLVED;
+	}
 }

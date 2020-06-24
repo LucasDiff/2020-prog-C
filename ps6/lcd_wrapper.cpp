fb8bb8904d33fe0c6a63b9eea388698747823944
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-06-24 22:31:45 +0200

    Update lcd_wrapper.cpp

--- ps6/lcd_wrapper.cpp
+++ ps6/lcd_wrapper.cpp
@@ -1,43 +1,42 @@
-#include "lcd_wrapper.h"
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include "lcd_wrapper.h"
 
-GMButton::GMButton(int pin) {
-  _pin = pin;
-  pinMode(_pin, INPUT);
-  digitalWrite(_pin, HIGH);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+void lcd_init(){
+  lcd.init();
+  lcd.backlight();
+  lcd.begin(16, 2);
+  lcd.print("Ty to zvladnes!");
 }
 
-bool GMButton::isButtonPressed() {
-  if (digitalRead(_pin) == LOW) {
-    return true;
-  } else {
-    return false;
-  }
+void lcd_clear(){
+  lcd.clear();
 }
 
-void GMButton::waitButtonRelease() {
-  delay(10); // debounce button press
-  while (digitalRead(_pin) == LOW);
-  delay(10); // debounce button release
+
+void lcd_set_cursor(int column, int row){
+  lcd.setCursor(column, row);
 }
 
-int GMButton::waitButtonAction(int alt_pin_1, int alt_pin_2) {
-  delay(10); // debounce button press
-  int action = NO_ACTION;
-  while (true) {
-    if (digitalRead(_pin) == HIGH) {
-      return BUTTON_RELEASE;
-    } else if (alt_pin_1 > 0 && digitalRead(alt_pin_1) == LOW) {
-      delay(10);
-      while (digitalRead(alt_pin_1) == LOW);
-      delay(10);
-      return BUTTON_ALT_1;
-    } else if (alt_pin_2 > 0 && digitalRead(alt_pin_2) == LOW) {
-      delay(10);
-      while (digitalRead(alt_pin_2) == LOW);
-      delay(10);
-      return BUTTON_ALT_2;
-    }
-  }
+
+void lcd_print(const char* text){
+  lcd.print(text);
+}
+
+void lcd_print(char c){
+  lcd.print(c);
+}
+
+void lcd_print_at(int column, int row, const char* text){
+    lcd_set_cursor(column, row);
+    lcd_print(text);
+}
+
+void lcd_print_at(int column, int row, char c) {
+    lcd_set_cursor(column, row);
+    lcd_print(c);
 }

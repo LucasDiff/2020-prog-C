64d74ded5661739f62f49fa5d1fa9f82170fc50f
Lukáš Németh <lukas.nemeth.2@student.tuke.sk>
2020-06-24 22:29:50 +0200

    Update ps6.ino

--- ps6/ps6.ino
+++ ps6/ps6.ino
@@ -1,34 +1,21 @@
-#include "mastermind.h"
 #include <Arduino.h>
-
-
+#include "mastermind.h"
+#include "lcd_wrapper.h"
 
 
 void setup() {
+
   randomSeed(analogRead(A0) + analogRead(A1) + analogRead(A2));
 
-  lcd.init();
-  lcd.backlight();
-  lcd.begin(16, 2);
-  lcd.print("Ty to zvladnes!");
-
-  pinMode(LED_BLUE_1, OUTPUT);
-  pinMode(LED_BLUE_2, OUTPUT);
-  pinMode(LED_BLUE_3, OUTPUT);
-  pinMode(LED_BLUE_4, OUTPUT); 
-  pinMode(LED_RED_1, OUTPUT);
-  pinMode(LED_RED_2, OUTPUT); 
-  pinMode(LED_RED_3, OUTPUT);
-  pinMode(LED_RED_4, OUTPUT);
-  turn_leds_off();
+  lcd_init();
+  leds_init();
 }
 
 
 void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(" ");
+  lcd_clear();
 
-  char *kod = generate_code(false, 4);
+  char *kod = generate_code(false, POCET_KOLIKOV);
   play_game(kod);
   while (true);
 }
